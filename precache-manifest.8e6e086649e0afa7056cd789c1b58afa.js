5a1bbdd43247e0cbb0a89de88d650ddadd8f65e5
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-20 16:24:07 -0700

    Updates

---
+++ precache-manifest.8e6e086649e0afa7056cd789c1b58afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b667a80ed2e066376fbcb2b8c5d09f11",
+    "url": "/Not-a-Twitter-Ripoff/index.html"
+  },
+  {
+    "revision": "015b17f60e543570d27a",
+    "url": "/Not-a-Twitter-Ripoff/static/css/main.a2dd1b0e.chunk.css"
+  },
+  {
+    "revision": "03782201b326b7767286",
+    "url": "/Not-a-Twitter-Ripoff/static/js/2.965de165.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Not-a-Twitter-Ripoff/static/js/2.965de165.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015b17f60e543570d27a",
+    "url": "/Not-a-Twitter-Ripoff/static/js/main.7fb484b7.chunk.js"
+  },
+  {
+    "revision": "e4cf4bf54f614f3064ad",
+    "url": "/Not-a-Twitter-Ripoff/static/js/runtime-main.0ddaadb3.js"
+  },
+  {
+    "revision": "8ac54729d081192c0d12127473f6e569",
+    "url": "/Not-a-Twitter-Ripoff/static/media/background3.8ac54729.png"
+  }
+]);
