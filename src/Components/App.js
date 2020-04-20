43fa653d5dc7084507553b1795643e2ced4b235f
Geoff Goetz <geoff.goetz503@gmail.com>
2020-04-20 15:32:52 -0700

    add heade.js and main components to app.js
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MainFeed from './MainFeed/MainFeed';
+import Header from './Header';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <MainFeed />
+    </React.Fragment>
+  )
+ }
+
+export default App;
